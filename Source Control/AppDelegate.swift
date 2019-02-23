8da22ee6b38ea9bdef81c8a8203b2eaa668a6af6
dalpac10 <dalpac10@yahoo.com>
2019-02-23 16:09:41 -0500

    Initial Commit

---
+++ Source Control/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Source Control
+//
+//  Created by Dennis Packer on 2/23/19.
+//  Copyright © 2019 DPG. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Source Control/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Source Control
+//
+//  Created by Dennis Packer on 2/23/19.
+//  Copyright © 2019 DPG. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
